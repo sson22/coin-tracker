10fecd556023e57dd8700da342900c25c6de63d9
sson22 <sophiesbson@gmail.com>
2022-06-12 12:41:14 +1000

    Price item added

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,11 +1,12 @@
 import { useQuery } from "react-query";
 import styled from "styled-components";
-import { fetchCoinHistory } from "../api";
+import { fetchCoinHistory, fetchCoinTickers } from "../api";
 const Overview = styled.div`
   display: flex;
   flex-direction: column;
   align-items: left;
   height: 200px;
+  padding: 40px;
   justify-content: space-between;
   border-radius: 10px;
   background-color: rgba(0, 0, 0, 0.5);
@@ -16,14 +17,60 @@ const PriceBlock = styled.li`
 interface PriceProps {
   coinId: string;
 }
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Price({ coinId }: PriceProps) {
+  const { isLoading, data } = useQuery<IPriceData>(
+    ["price", coinId],
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 10000,
+    }
+  );
   return (
     <Overview>
       <ul>
-        <PriceBlock>1</PriceBlock>
-        <PriceBlock>2</PriceBlock>
-        <PriceBlock>3</PriceBlock>
-        <PriceBlock>4</PriceBlock>
+        <PriceBlock>${data?.quotes.USD.price.toFixed(2)}</PriceBlock>
+        <PriceBlock>
+          Change over 1h:{data?.quotes.USD.percent_change_1h}%
+        </PriceBlock>
+        <PriceBlock>
+          Change over 24h:{data?.quotes.USD.percent_change_24h}%
+        </PriceBlock>
+        <PriceBlock>Last updated at {data?.last_updated}</PriceBlock>
       </ul>
     </Overview>
   );
